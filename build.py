d798b252c0e64862ef6a6ee173e5fae71b37ce6c
gfdgd_xi <3025613752@qq.com>
2024-04-20 15:28:29 +0800

    新增图标

--- build.py
+++ build.py
@@ -48,8 +48,6 @@ def Write(path: str):
         <hr/>
         <h3>更新时间：{datetime.datetime.now().strftime("%Y年%m月%d日 %H:%M:%S")}</h3>
         <hr/>
-        <h2>添加 apt 源</h2>
-        <pre><code>wget http://dde15.debian12.gfdgdxi.top/sources/github.sh ; bash github.sh ; rm github.sh</code></pre>
         <p>项目地址：<a href="https://gitee.com/GXDE/">https://gitee.com/GXDE/</a></p>
         <p><a href="https://github.com/GXDE/">https://github.com/GXDE/</a></p>
         <hr/>
